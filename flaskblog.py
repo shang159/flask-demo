10204bd665bf026b54fca56b89631ea33f12c76a
shang159 <1075403539@qq.com>
2019-07-06 15:04:18 +0800

    单独的模板，没有继承关系

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template
 
 flaskblog=Flask(__name__)
 
+posts=[
+    {'author':'skeep588','title':'Blog Post 1',
+        'content':'First post conte','date_posted':"April 20,2018"},
+    {'author': 'skeep1','title': 'Blog Post 2',
+        'content': 'First post conte1','date_posted': "April 20,2018"},
+    {'author': 'skeep2','title': 'Blog Post 3',
+        'content': 'First post conte3','date_posted': "April 20,2018"},
+    {'author': 'skeep3','title': 'Blog Post 4',
+     'content': 'First post conte2','date_posted': "April 20,2018"}
+]
 
 
 @flaskblog.route("/")
-def hello():
-    return "hello world"
+@flaskblog.route("/home")
+def home():
+    return render_template('home.html',posts=posts,title="Home")
+
+@flaskblog.route("/about")
+def about():
+    return render_template("about.html")
+
 
 #两种启动方式：
 ##利用python-dotenv   flask run
 ##利用app.run
-
-
 
 if __name__ == "__main__":
     flaskblog.run(debug=True)
